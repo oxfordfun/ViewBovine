f0e3168ec25bb6a6d0147258050a9943d3fb9bff
Denis Volk <denis.volk@gmail.com>
2018-12-05 15:38:42 +0000

    wip

--- main.py
+++ main.py
@@ -6,25 +6,38 @@ from flask import Flask, render_template, abort, request
 
 myapp = Flask(__name__)
 
+def setup_logging():
+    #
+    # setup global logger
+    #
+    glogger = logging.getLogger("fan_logger")
+    glogger.setLevel(logging.DEBUG)
+    c_handler = logging.StreamHandler()
+    c_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    glogger.addHandler(c_handler)
+
+setup_logging()
+logger = logging.getLogger("fan_logger")
+    
 def call_api(kind, path):
     if kind == 'tree':
         host = 'http://192.168.7.30:5008'
     if kind == 'map':
         host = 'http://192.168.7.30:5006'
 
-    logging.warning('=> {0}{1}'.format(host, path))
+    logger.debug('=> {0}{1}'.format(host, path))
 
     try:
         req_api = requests.get(host + path)
     except Exception as e:
         abort(500, description=e)
 
-    logging.warning('<= {0}'.format(req_api.text)[80:])
+    logger.debug('<= {0}'.format(req_api.text)[:80])
 
     return req_api.text
 
-def get_neighbours(guid, distance=6, quality=80):
-    ret = call_api('tree', '/neighbours2/{0}?reference=R00000039&distance={1}&quality=0.8'.format(guid, distance, quality))
+def get_neighbours(guid, distance=6, quality="0.8"):
+    ret = call_api('tree', '/neighbours2/{0}?distance={1}&quality={2}&reference=R00000039'.format(guid, distance, quality))
     return ret
 
 @myapp.route('/')
@@ -48,7 +61,6 @@ def sample_map():
     for guid,_ in names_guids[1:]:
         other_guids.append(guid)
 
-    print(my_guid)
 
     req_coord = requests.get('http://192.168.7.30:5006/coordinates2/{0}'.format(sample_name))
     data = json.loads(req_coord.text)[0]
@@ -61,39 +73,47 @@ def sample_map():
 
     req_movement = requests.get('http://192.168.7.30:5006/api/locations/{0}'.format(sample_name))
     movement_data = json.loads(req_movement.text)['data'][sample_name]
-    print(movement_data)
 
     return render_template('map.template', 
-        sample_name = sample_name,
-        sample_guid = my_guid,
-        eartag = eartag,
-        map_x = map_x,
-        map_y = map_y,
-        herd_id = herd_id,
-        other_guids = other_guids,
-        movement_data = movement_data
-        )
+                           sample_name = sample_name,
+                           sample_guid = my_guid,
+                           eartag = eartag,
+                           map_x = map_x,
+                           map_y = map_y,
+                           herd_id = herd_id,
+                           other_guids = other_guids,
+                           movement_data = movement_data
+    )
 
 @myapp.route('/sample/neighbour/')
 def sample_neighbour():
     my_guid = request.args.get("sample_guid")
     my_distance = request.args.get('distance')
     my_quality = request.args.get('quality')
-    print(my_guid)
     if my_distance and my_quality:
+        # [neighbour, distance]
         neighbours = get_neighbours(my_guid, my_distance, my_quality)
+
+        # get sample names
+        neighbour_guids = [x[0] for x in neighbours]
+        neighbour_guids = ",".join(neighbour_guids)
+        call_api('tree', '/lookup/{0}'.format(neighbour_guids))
+
+
     else:
         neighbours = list()
 
-    logging.warning(neighbours)
-    coordinate_query = ",".join([neighbour[1] for neighbour in neighbours])
+    
+
+    # do coordinate lookup on names
+
+
     data = call_api('map', '/coordinates2/{0}'.format(coordinate_query))
-    logging.warning(data)
 
     return render_template('neighbour.template',
-        sample_guid = my_guid,
-        sample_name = "asdf",
-        neighbours = neighbours
+                           sample_guid = my_guid,
+                           sample_name = "asdf",
+                           neighbours = neighbours
     )
 
 myapp.run()
