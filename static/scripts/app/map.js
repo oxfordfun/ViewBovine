896e8537a45866dbfb4bd4fbae8c5099e79746c2
Fan Yang <fan.yang.oxford@gmail.com>
2020-12-08 14:54:48 +0000

    adjust tile size

--- static/scripts/app/map.js
+++ static/scripts/app/map.js
@@ -29,6 +29,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
     'Imagery  <a href="https://www.mapbox.com/">Mapbox</a>',   
     id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
 }).addTo(mymap);
 
 
